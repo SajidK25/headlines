965f187e6f36811fee30ecbfbdca68fceac73325
Sajid Khan <sajidkhan@gmx.us>
2016-10-10 00:55:34 +0600

    dynamic routing

--- headlines.py
+++ headlines.py
@@ -1,11 +1,30 @@
 from flask import Flask
+import feedparser
+
 
 app=Flask(__name__)
+RSS_FEEDS = {'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
+             'cnn':'http://rss.cnn.com/rss/edition.rss',
+             'fox':'http://feeds.foxnews.com/foxnews/latest',
+             'iol':'http://www.iol.co.za/cmlink/1.640'}
+
+
+
 
 
 @app.route('/')
-def get_news():
-	return "no news yet"
+@app.route('/<publication>')
+def get_news(publication):
+	feed=feedparser.parse(RSS_FEEDS[publication])
+	first_article=feed['entries'][0]
+	return '''<html>
+	   <body>
+	    <h1>BBC headlines:</h1>
+	    <i>{0}</i></br>
+	    <b>{1}</b></br>
+	    <p>{2}</p>
+	   </body>
+	</htmL>'''.format(first_article.get("title"),first_article.get("published"),first_article.get("summary"))
 
 if __name__=='__main__':
 	app.debug=True
